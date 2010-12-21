6b05bd239781bdb019b81c33e3c0259ebbfaea64
jb-tester <petrovskaya@gmail.com>
2010-12-21 18:19:40 +0300

    add first file (aaa.java)

---
+++ root/src/qwe/rty/AAA.java
@@ -0,0 +1,11 @@
+package qwe.rty;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ssh1
+ * Date: 12/21/10
+ * Time: 6:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AAA {
+}
