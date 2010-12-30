7a15e316febe5baa5f2aaceb06bbee7f2a1a290f
irina <Irina.Petrovskaya@jetbrains.com>
2010-12-30 14:31:03 +0300

    edit AAA

--- root/src/qwe/rty/AAA.java
+++ root/src/qwe/rty/AAA.java
@@ -1,12 +1,6 @@
 package qwe.rty;
 
-/**
- * Created by IntelliJ IDEA.
- * User: ssh1
- * Date: 12/21/10
- * Time: 6:17 PM
- * To change this template use File | Settings | File Templates.
- */
+// ip
 public class AAA {
     int i1;
     int i2;
